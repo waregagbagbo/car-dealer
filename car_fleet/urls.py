63384047f35b9a8ca4d119a1e060beac8d8cc9eb
waregagbagbo <gabelwarega@gmail.com>
2022-11-03 16:54:24 +0300

    fixed detailview and the associate url

--- car_fleet/urls.py
+++ car_fleet/urls.py
@@ -1,10 +1,11 @@
 from nturl2path import url2pathname
 from django.contrib import admin
 from django.urls import path
-from .views import CarListView
+from .views import CarListView,CarDetailAccessView
 
 app_name = 'car_fleet'
 
 urlpatterns = [
     path('',CarListView.as_view(), name='home'),
+    path('<int:pk>/',CarDetailAccessView.as_view(), name='vehicle'),
 ]
--- car_fleet/views.py
+++ car_fleet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 
@@ -16,4 +17,9 @@ class CarListView(ListView):
         context = super(CarListView, self).get_context_data(**kwargs)
         context['listing'] = Car.objects.all()
         return context
+    
+
+class CarDetailAccessView(DetailView, LoginRequiredMixin):
+    model = Car
+    template_name ='page/car_detail.html'
 
