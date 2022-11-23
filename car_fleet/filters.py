3f353c2d21048c21dee00a9f7bef0720573a3493
waregagbagbo <gabelwarega@gmail.com>
2022-11-23 17:23:53 +0300

    successfully enabled django filter forms

--- car_fleet/filters.py
+++ car_fleet/filters.py
@@ -2,11 +2,13 @@ from .models import *
 import django_filters
 
 class CarFilter(django_filters.FilterSet):
-    year_manufactured = django_filters.NumberFilter(field_name='release_year')
+    category = django_filters.ChoiceFilter(field_name='car_category',choices=CAR_CATEGORY)
+    
+   # year_manufactured = django_filters.NumberFilter(field_name='release_year')
     
     class Meta:
        model = Car
-       fields = ['make','model_type','price','color','fuel']
+       fields = ['category','make','model_type','price','color','fuel']
 
     
   
