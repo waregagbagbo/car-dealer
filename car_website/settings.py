59375c6c2db7d611b014a5ca4edf1293141846d8
waregagbagbo <gabelwarega@gmail.com>
2022-12-28 20:38:40 +0300

    updated the settings file

--- car_website/settings.py
+++ car_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-le5g5ag^x%2j&t2=5axl^ogqjtzy_ojo*i$q@mkk#mr%3#m0r8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG  = True
+DEBUG  = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
